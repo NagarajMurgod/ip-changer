7d107eedffa3b12308088b4d15942c9387605450
Nagaraj Murgod <01fe19bee418@gmail.com>
2024-11-17 17:54:19 +0530

    enahancement

--- ipchanger.py
+++ ipchanger.py
@@ -4,6 +4,7 @@ from stem.control import Signal
 from stem.control import Controller
 import sys
 from requests import get
+import subprocess
 
 banner = '''
 ██╗██████╗        ██████╗██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███████╗██████╗ 
@@ -20,24 +21,65 @@ GREEN = '\033[32;1m'
 RED = '\033[31;1m'
 WHITE = '\033[m'
 
-print('--'*12 + 'WELL-COME-TO' + '--'*12)
+print('--'*15 + 'WELL-COME-TO' + '--'*15)
 print(banner)
 print('created by : 14M5K1D')
-print('--'*30)
+print('--'*40)
 
-class changeip:
+class ChangeIp:
 
-    def __init__(self,passwd,change,intervals):
+    def __init__(self,passwd="Torpassword@123",change=50,intervals=60):
         self.password=passwd
         self.intervals=intervals
         self.change=change
 
 
     def check_pkg(self):
-        if os.system('which tor > /dev/null ')!=0:
+        if os.system('which tor > /dev/null ') != 0:
             sys.exit('[*] tor is not installed..! , install it by "sudo apt-get install tor" ')
         else:
             pass
+
+    # def open_firefox(self):
+
+    
+    def __get_hash_password(self):
+        command = ['tor', '--hash-password', self.password ]
+        try:
+            result = subprocess.run(command, capture_output=True, text=True)
+            
+            if result.stdout:
+                return result.stdout.strip().split()[-1]
+            
+            if result.error:
+                sys.exit(f"[!]{RED}{e}")
+
+        except Exception as e:
+            sys.exit(f"{RED} Failed to generated hash password due to error : {e}")
+            
+
+    def _configure_torrc(self):
+        
+        print(f'{WHITE}[*] Configuring the torrc file....!')
+        torrc_file_path = "/etc/tor/torrc"
+        hash_password = self.__get_hash_password()
+        try:
+            with open(torrc_file_path, 'r') as fl:
+                lines = fl.readlines()
+
+            for i in range(len(lines)):
+                if "ControlPort 9051" in lines[i]:
+                    lines[i] = "ControlPort 9051"
+                
+                if "HashedControlPassword" in lines[i]:
+                    lines[i] = f"HashedControlPassword {hash_password}"
+
+            with open(torrc_file_path, 'w') as file:
+                file.writelines(lines)
+
+            print(f'{GREEN}[*] Torrc file is configured succesfully')
+        except Exception as e:
+            print(e)
 
 
     def show_ip(self):
@@ -67,7 +109,7 @@ class changeip:
         print('[*] checking for internet connetcion....!')
         time.sleep(2)
         if not os.system('ping -c 2 google.com > /dev/null '):
-            print('[*] CONNECTED')
+            print(f'{GREEN}[*] CONNECTED')
             self.start_tor()
         else:
             sys.exit('[*] Connection failed...! Please check your internet..!')
@@ -75,6 +117,8 @@ class changeip:
 
     def start_tor(self):
         self.check_pkg()
+        self._configure_torrc()
+
         try:
             if not os.system('service tor start'):
                 time.sleep(1)
@@ -83,11 +127,13 @@ class changeip:
         except Exception as e:
             print('Oops ..!,Something went wrong')
             print(str(e))
+            os.system('service tor stop')
             sys.exit()
         except KeyboardInterrupt:
             os.system('service tor stop')
 
     def _renew_ip(self):
+        print(WHITE)
         try: 
             for i in range(0,self.change-1):
                 print('your ip adress is : ',self.show_ip())
@@ -102,68 +148,75 @@ class changeip:
 
         except Exception as error:
             print(str(error))
-            sys.exit('unable to connected to port 9051, [!] FAILED')
+            os.system('service tor stop')
+            sys.exit(f'[!]{RED}unable to connected to port 9051')
 
-interval = '20 seconds'
-password = None
-change = '0 times'
+    def start_ip_changer(self):
+        self._check_root()
 
-def main():
-    def setcmd(option, value):
-        global interval, password, change
-        if option.lower() == 'interval':
-            if not value.isdigit():
-                print(f'{RED}[!] ERROR: {WHITE}Please enter a number.')
-            else:
-                if not int(value)>=20:
-                    print(f'[!] ERROR: {WHITE}Please enter a number which is greater than or equal to 20.')
-                else:
-                    interval = f'{value} seconds'
-                    print(f'{GREEN} {option} ==> {value} seconds {WHITE}')
-        elif option.lower() == 'password':
-            password = value
-            print(f'{GREEN} {option} ==> {value} {WHITE}')
-        elif option.lower() == 'times_to_change':
-            if not value.isdigit():
-                print(f'{RED}[!] ERROR: {WHITE}Please enter a number.')
-            else:
-                if int(value) > 200:
-                    print(f'{RED}[!] ERROR: {WHITE}You can only change IP for 200 times max.')
-                else:
-                    change = f'{value} times'
-                    print(f'{GREEN} {option} ==> {value} times {WHITE}')
-        else:
-            print(f'{RED}ERROR: INVALID OPTION SELECTED.{WHITE}')
-    while True:
-        x = input(f'{WHITE}[{RED}IP-CHANGER{WHITE}] : ')
-        if 'set' in x.lower() and len(x.split(' ')) > 1:
-            setcmd(x.split(' ')[1],x.split(' ')[2])
-        elif 'options' in x.lower():
-            print('----OPTIONS----\t----VALUE----\t----DESCRIPTION----')
-            print(f'interval\t{interval}\tinterval between changes (in seconds).')
-            print(f'password\t{password}\tthe password to authenticate with tor(the passsword you used to create the hashed password)')
-            print(f'times_to_change\t{change}\tnumber of times you want to change your ip (max is 200)\n')
-        elif 'help' in x.lower():
-            print('---COMMANDS--\t---INFO---')
-            print('set\tset a option {set <option name> <value>')
-            print('run\trun the ip changer with the available options.')
-            print('options\tshow available options and their current values.')
-            print('help]tshow this help message')
-            print('exit\texit the script.')
-        elif 'run' in x.lower():
-            if password != None and change != '0 times':
-                print(f'proceeding with the options:-\ninterval = {interval}\npassword = {password}\ntimes_to_change = {change}\n')
-                try:
-                    run = changeip(password,int(change.split(' ')[0]),int(interval.split(' ')[0]))
-                    run._check_root()
-                except Exception as e:
-                    print(f'{RED} Some Error Occoured:- %s\n Exitting script...'%(e.args))
-                    os.system('service tor stop')
-                    exit()
-            else:
-                print(f'{RED}ERROR: {WHITE}Some of the options are not set.')
-        elif 'exit' in x.lower():
-            exit()
-        else:
-            print(f'{RED}ERROR: {WHITE} Invalid Command.')
-main()
+obj = ChangeIp(change=10,intervals=20)
+obj.start_ip_changer()
+
+# interval = '20 seconds'
+# password = None
+# change = '0 times'
+
+# def main():
+#     def setcmd(option, value):
+#         global interval, password, change
+#         if option.lower() == 'interval':
+#             if not value.isdigit():
+#                 print(f'{RED}[!] ERROR: {WHITE}Please enter a number.')
+#             else:
+#                 if not int(value)>=20:
+#                     print(f'[!] ERROR: {WHITE}Please enter a number which is greater than or equal to 20.')
+#                 else:
+#                     interval = f'{value} seconds'
+#                     print(f'{GREEN} {option} ==> {value} seconds {WHITE}')
+#         elif option.lower() == 'password':
+#             password = value
+#             print(f'{GREEN} {option} ==> {value} {WHITE}')
+#         elif option.lower() == 'times_to_change':
+#             if not value.isdigit():
+#                 print(f'{RED}[!] ERROR: {WHITE}Please enter a number.')
+#             else:
+#                 if int(value) > 200:
+#                     print(f'{RED}[!] ERROR: {WHITE}You can only change IP for 200 times max.')
+#                 else:
+#                     change = f'{value} times'
+#                     print(f'{GREEN} {option} ==> {value} times {WHITE}')
+#         else:
+#             print(f'{RED}ERROR: INVALID OPTION SELECTED.{WHITE}')
+#     while True:
+#         x = input(f'{WHITE}[{RED}IP-CHANGER{WHITE}] : ')
+#         if 'set' in x.lower() and len(x.split(' ')) > 1:
+#             setcmd(x.split(' ')[1],x.split(' ')[2])
+#         elif 'options' in x.lower():
+#             print('----OPTIONS----\t----VALUE----\t----DESCRIPTION----')
+#             print(f'interval\t{interval}\tinterval between changes (in seconds).')
+#             print(f'password\t{password}\tthe password to authenticate with tor(the passsword you used to create the hashed password)')
+#             print(f'times_to_change\t{change}\tnumber of times you want to change your ip (max is 200)\n')
+#         elif 'help' in x.lower():
+#             print('---COMMANDS--\t---INFO---')
+#             print('set\tset a option {set <option name> <value>')
+#             print('run\trun the ip changer with the available options.')
+#             print('options\tshow available options and their current values.')
+#             print('help]tshow this help message')
+#             print('exit\texit the script.')
+#         elif 'run' in x.lower():
+#             if password != None and change != '0 times':
+#                 print(f'proceeding with the options:-\ninterval = {interval}\npassword = {password}\ntimes_to_change = {change}\n')
+#                 try:
+#                     run = changeip(password,int(change.split(' ')[0]),int(interval.split(' ')[0]))
+#                     run._check_root()
+#                 except Exception as e:
+#                     print(f'{RED} Some Error Occoured:- %s\n Exitting script...'%(e.args))
+#                     os.system('service tor stop')
+#                     exit()
+#             else:
+#                 print(f'{RED}ERROR: {WHITE}Some of the options are not set.')
+#         elif 'exit' in x.lower():
+#             exit()
+#         else:
+#             print(f'{RED}ERROR: {WHITE} Invalid Command.')
+# main()
